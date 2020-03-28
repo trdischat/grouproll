8d249860401ac0d6cbb729fbe30d14c80b7aa43d
tsrgit <trgit@hoghill.net>
2020-03-28 16:28:50 -0700

    eliminate patches in favor of function replacement

--- scripts/patches.js
+++ scripts/patches.js
@@ -1,46 +1,42 @@
-function patchClass(klass, func, line_number, line, new_line) {
-  let funcStr = func.toString();
-  let lines = funcStr.split("\n");
-  if (lines[line_number].trim() == line.trim()) {
-    lines[line_number] = lines[line_number].replace(line, new_line);
-    classStr = klass.toString();
-    fixedClass = classStr.replace(funcStr, function(m) {return lines.join("\n")});
-    return Function('"use strict";return (' + fixedClass + ')')();
-  }
-  else {
-    console.log("Function has wrong content at line ", line_number, " : ", lines[line_number].trim(), " != ", line.trim(), "\n", funcStr);
-  }
-}
-
-/* Patch Die class to allow limit on number of dice to reroll (e.g.,
- * a halfing rolling an attack with advantage would use '2d20r1=1kh' to roll
- * two d20, reroll on a 1, but only on one die, then keep the highest roll).
+/* Allow limit on number of dice to reroll (e.g., a halfing rolling an attack 
+ * with advantage would use '2d20r1=1kh' to roll two d20, reroll on a 1, but
+ * only on one die, then keep the highest roll).
  */
-function patchDieClass() {
-  newClass = patchClass(Die, Die.prototype.reroll, 0,
-    `reroll(targets) {`,
-    `reroll(targets, maxrerolls) {`);
-  if (!newClass) return;
-  newClass = patchClass(newClass, newClass.prototype.reroll, 3,
-    `// Flag dice which are eligible for re-roll`,
-    `maxrerolls = maxrerolls || 999;
-    // Flag dice which are eligible for re-roll`);
-  if (!newClass) return;
-  newClass = patchClass(newClass, newClass.prototype.reroll, 7,
-    `else if ( targets.includes(r.roll) ) return r.rerolled = true;`,
-    `else if ( targets.includes(r.roll) && maxrerolls > 0 ) { maxrerolls--; return r.rerolled = true; }`);
-  if (!newClass) return;
-  newClass = patchClass(newClass, Die.prototype._applyReroll, 15,
-    `this.reroll(target);`,
-    `this.reroll(target, rr[3]);`);
-  if (!newClass) return;
-  newClass.rgx = Die.rgx;
-  newClass.rgx.reroll = /r(<=|>=|<|>)?([0-9]+)?(?:=([0-9]+))?/;
-  Die = newClass;
-  CONFIG.diceTypes[CONFIG.diceTypes.findIndex(d => d.name === "Die")] = Die
+Die.prototype.reroll = function(targets, maxrerolls = 999) {   // ! Changed line
+  if ( !targets || !targets.length ) return this.rolls;
+
+  // Flag dice which are eligible for re-roll
+  let eligible = this.rolls.filter(r => {
+    if ( r.rerolled || r.discarded ) return false;
+    else if ( targets.includes(r.roll) && maxrerolls > 0 ) {   // ! Changed line
+      maxrerolls--;
+       return r.rerolled = true;
+    }
+    return false;
+  });
+
+  // Roll any eligible dice
+  let rolls = eligible.map(r => this._roll());
+  this.rolls = this.rolls.concat(rolls);
+  return this;
 }
 
-if (patchedDieClass == undefined) {
-  patchDieClass();
-  var patchedDieClass = true;
+Die.prototype._applyReroll = function(option) {
+  let rr = option.match(Die.rgx.reroll);
+  if ( !rr ) return;
+
+  // Determine the reroll range
+  let target, nrr = parseInt(rr[2] || 1);
+  if ( rr[1] ) {
+    if ( rr[1] === "<" )        target = Array.fromRange(nrr);
+    else if ( rr[1] === "<=" )  target = Array.fromRange(nrr).map(n => n + 1);
+    else if ( rr[1] === ">" )   target = Array.fromRange(this.faces - nrr).map(n => n + nrr + 1);
+    else if ( rr[1] === ">=" )  target = Array.fromRange(this.faces - nrr + 1).map(n => n + nrr);
+  }
+  else target = [nrr];
+
+  // Reroll the die
+  this.reroll(target, rr[3]);                                  // ! Changed line
 }
+
+Die.rgx.reroll = /r(<=|>=|<|>)?([0-9]+)?(?:=([0-9]+))?/;
