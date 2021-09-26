cc5f63fc3f59673631895e4c417d92f98233ef62
Taylor Reid <trgit@hoghill.net>
2021-09-26 12:20:58 -0700

    add auto roll to chat option

--- module/settings.js
+++ module/settings.js
@@ -10,6 +10,15 @@ export const registerSettings = function () {
         config: true,
         onChange: s => { }
     });
+    game.settings.register("grouproll", "send2chat", {
+        name: "Output Rolls to Chat",
+        hint: "The default behavior when this option is set to false is to hide rolls from the chat window; Shift-Click the Roll button to send rolls to chat.  To reverse this behavior, set this option to true to automatically output all rolls to the chat window; Shift-Click the Roll button to hide rolls from chat.",
+        scope: "world",
+        type: Boolean,
+        default: false,
+        config: true,
+        onChange: debouncedReload
+    });
     game.settings.register("grouproll", "averageRolls", {
         name: "Average Rolls",
         hint: "This personal house rule uses the average of 2d20 in place of 1d20 for NORMAL rolls. Outside of the Group Roll app, this setting only applies to the D&D 5e system and is disabled when using the BetterRolls5e module.",
