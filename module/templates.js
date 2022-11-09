ffc774dff7eeabf4e97d893cf984241eb3ea9ff5
Taylor Reid <trgit@hoghill.net>
2022-11-08 20:44:55 -0800

    restore full list of preloaded templates

--- module/templates.js
+++ module/templates.js
@@ -1,5 +1,6 @@
 export const preloadTemplates = async function () {
     const templatePaths = [
+        'modules/grouproll/templates/group-app-window.html',
         'modules/grouproll/templates/group-chat-roll.html',
         'modules/grouproll/templates/group-chat-tooltip.html',
         'modules/grouproll/templates/group-ability-check.html',
