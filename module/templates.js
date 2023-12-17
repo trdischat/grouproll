3f1b693553abcb21e19084f15d28d8b7fcd1ee98
Taylor Reid <trgit@hoghill.net>
2023-12-17 15:20:34 -0800

    do not load pf2e templates

--- module/templates.js
+++ module/templates.js
@@ -4,9 +4,7 @@ export const preloadTemplates = async function () {
         'modules/grouproll/templates/group-chat-roll.html',
         'modules/grouproll/templates/group-chat-tooltip.html',
         'modules/grouproll/templates/group-ability-check.html',
-        'modules/grouproll/templates/group-ability-check-pf2e.html',
-        'modules/grouproll/templates/group-skill-check.html',
-        'modules/grouproll/templates/group-skill-check-pf2e.html'
+        'modules/grouproll/templates/group-skill-check.html'
     ];
     return loadTemplates(templatePaths);
 };
